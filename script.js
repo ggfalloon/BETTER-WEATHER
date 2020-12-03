ddcb77cb76e6f135d19ca2ea087e4bd37478814d
Gabriele Falloon <ggfalloon@gmail.com>
2020-12-03 10:46:24 -0600

    updated fonts and styling

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
+    // Ajax call to get data from the odds api
     $.ajax({
-        url: "https://api.the-odds-api.com/v3/odds/?apiKey=a2fcf2e355a2d98481fc8e5cf46d6a39&sport=americanfootball_nfl&region=uk&mkt=h2h",
+        url: "https://api.the-odds-api.com/v3/odds/?apiKey=b4c5876ff0d27e337508915a11e59704&sport=americanfootball_nfl&region=uk&mkt=h2h",
         method: "GET"
     }).then(function (oddsResponse) {
         console.log(oddsResponse)
-
+        // Pulls all upcoming home teams and display them on the page
         var dataArr = oddsResponse.data;
         for (i = 0; i < dataArr.length; i++) {
             // Changed to pull only home Teams
@@ -13,64 +14,25 @@ $(document).ready(function () {
             $("#data").append(allTeams + " <br> ");
 
         }
-
+        // Added click function for search input
         $("#searchBtn").click(function () {
-
-
-            var cityName = $("#teamName").val();
-            var appID = "21292f97c006ec7feb138c594d793fed";
-            var queryURL = "https://api.openweathermap.org/data/2.5/forecast/?q=" + cityName + "&units=imperial&appid=" + appID;
-
-
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                // Added City name to weather
-                var weatherCity = response.city.name;
-                for (i = 0; i < response.city.name.length; i++) {
-                    $("#weatherCity").html(weatherCity);
-                }
-
-                var dayList = " ";
-
-                for (i = 0; i < response.list.length; i++) {
-
-                    if (response.list[i].dt_txt.split(" ")[1] === "18:00:00") {
-
-                        dayList += "<br>";
-                        dayList += "<ul>";
-                        dayList += "<li >Date: " + moment(response.list[i].dt_txt.split(" ")[0], "YYYY-MM-DD").format("MM-DD-YYYY") + "</li>";
-                        dayList += "<li>" + "<img src='https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png'>" + "</li>";
-                        dayList += "<li>Temp: " + Math.trunc(response.list[i].main.temp) + "\u00B0</li>";
-                        dayList += "<li>Humidity: " + response.list[i].main.humidity + "%</li>";
-                        dayList += "<li>Wind Speed: " + response.list[i].wind.speed + "mph</li>";
-                        dayList += "<li> Visibility: " + response.list[i].visibility + "m</li>";
-                        dayList += "<li>Precipitation: " + response.list[i].pop + "\"\</li>";
-                        dayList += "</ul>";
-                        dayList += "<br>";
-                        dayList += "<hr>";
-                    }
-
-                } $("#weather").html(dayList);
-
-            });
+            var cityTeam = $("#teamName").val();
 
             var queryURL = "https://en.wikipedia.org/w/api.php?action=parse&pageid=10814816&prop=text&format=json";
             var pageHolder = "";
+            // Ajax request for wikipedia stadium table
             $.ajax({
                 url: queryURL,
                 method: "GET",
                 crossDomain: true,
                 dataType: 'jsonp',
             }).then(function (response) {
-
+                // Parses the table to an object array
                 var wikipage = response.parse.text["*"]
                 pageHolder = $("<div>").html(wikipage)
                 var wikiTable = pageHolder.find("table.wikitable.sortable")[0];
                 var tableRows = $(wikiTable).find("tr:not(tr:first-child)")
-                var stadiumArray = [];
-
+                // Loop through the table data and write dynamically to the page
                 $.each(tableRows, function (index, stadiumTR) {
                     var tdArray = $(stadiumTR).find("td, th");
                     var imgSrc = "https:" + tdArray[0].children[0].children[0].attributes[1].value;
@@ -78,7 +40,7 @@ $(document).ready(function () {
                         return $1 === '120px' ? '1200px' : '120px';
                     });
 
-                    if (cityName == tdArray[3].outerText.split(",")[0]) {
+                    if (cityTeam == tdArray[3].outerText.split(",")[0]) {
 
                         // $("#imageEl").html(imgSrc);
                         $("#stadiumEl").html(tdArray[1].outerText);
@@ -92,14 +54,46 @@ $(document).ready(function () {
                 });
             });
 
-            // $.ajax({
-            //     url: "https://api.the-odds-api.com/v3/odds/?apiKey=9affd49948bc48e9c0515a0456515a2f&sport=americanfootball_nfl&region=uk&mkt=h2h",
-            //     method: "GET"
-            // }).then(function (oddsResponse) {
-            //     console.log(oddsResponse)
 
-            // var dataArr = oddsResponse.data;
+            var cityName = $("#teamName").val();
+            var appID = "21292f97c006ec7feb138c594d793fed";
+            var queryURL = "https://api.openweathermap.org/data/2.5/forecast/?q=" + cityName + "&units=imperial&appid=" + appID;
 
+            // Ajax request for current searched city weather info
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                // Added City name to weather section
+                var weatherCity = response.city.name;
+                for (i = 0; i < response.city.name.length; i++) {
+                    $("#weatherCity").html(weatherCity);
+                }
+                var dayList = " ";
+
+                for (i = 0; i < response.list.length; i++) {
+                    // Removes time from date feature
+                    if (response.list[i].dt_txt.split(" ")[1] === "18:00:00") {
+                        // dynamically displays weather data
+                        dayList += "<br>";
+                        dayList += "<ul>";
+                        dayList += "<li >Date: " + moment(response.list[i].dt_txt.split(" ")[0], "YYYY-MM-DD").format("MM-DD-YYYY") + "</li>";
+                        dayList += "<li>" + "<img src='https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png'>" + "</li>";
+                        dayList += "<li>Temp: " + Math.trunc(response.list[i].main.temp) + "\u00B0</li>";
+                        dayList += "<li>Humidity: " + response.list[i].main.humidity + "%</li>";
+                        dayList += "<li>Wind Speed: " + response.list[i].wind.speed + "mph</li>";
+                        dayList += "<li> Visibility: " + response.list[i].visibility + "m</li>";
+                        dayList += "<li>Precipitation: " + response.list[i].pop + "\"\</li>";
+                        dayList += "</ul>";
+                        dayList += "<br>";
+                        dayList += "<hr>";
+                    }
+                    // Writes weather data to the page
+                } $("#weather").html(dayList);
+
+            });
+
+            // Loops through odds api array and writes dynamically to html
             for (i = 0; i < dataArr.length; i++) {
 
                 if (cityName == dataArr[i].home_team.split(" ")[0]) {
@@ -127,11 +121,10 @@ $(document).ready(function () {
 
                 }
             }
-
+            // Clears appended html upon new search
         }); $("#websites").html(" ");
 
     });
 
 });
 
-// });
